67022dbe73dea969afa957756d62dc03dc99bc97
Ruben Romero Montes <rromerom@redhat.com>
2019-09-19 17:54:30 +0200

    infinispan config
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- serverless-workflow/src/main/java/org/kiegroup/kogito/serverless/process/WorkflowProcess.java
+++ serverless-workflow/src/main/java/org/kiegroup/kogito/serverless/process/WorkflowProcess.java
@@ -1,11 +1,17 @@
 package org.kiegroup.kogito.serverless.process;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.kie.api.definition.process.Process;
 import org.kie.kogito.Config;
 import org.kie.kogito.Model;
+import org.kie.kogito.persistence.KogitoProcessInstancesFactory;
 import org.kie.kogito.process.ProcessInstance;
 import org.kie.kogito.process.impl.AbstractProcess;
 import org.kiegroup.kogito.serverless.model.JsonModel;
@@ -18,8 +24,10 @@ public class WorkflowProcess extends AbstractProcess<JsonModel> {
     WorkflowService workflowService;
 
     @Inject
-    public WorkflowProcess(Config config) {
+    public WorkflowProcess(Config config, RemoteCacheManager cacheManager) {
         super(config.process());
+        this.setProcessInstancesFactory(new KogitoProcessInstancesFactory(cacheManager) {
+        });
     }
 
     @Override
